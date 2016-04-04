72e453e5f7e67b005d9a2e4494910ab3920defc1
Amin Karaji <amin.karaji@siteminder.com>
2016-04-04 15:51:36 +1000

    More OOP

--- lib/scraper/parser.rb
+++ lib/scraper/parser.rb
@@ -1,10 +1,13 @@
 module Scraper
   class Parser
-    attr_accessor :url
-    attr_reader :browser
-
-    def initialize(url)
+    attr_accessor :url, :per_serving_unit, :per_serving_amount
+    attr_reader :browser, :nutrition_facts
+    
+    def initialize(url, per_serving_unit: 'grams', per_serving_amount: 100)
       @url = url
+      @nutrition_facts = []
+      @per_serving_amount = per_serving_amount
+      @per_serving_unit = per_serving_unit
       raise ArgumentError.new("url cannot be blank") if url.nil?
     end
 
@@ -12,20 +15,19 @@ module Scraper
       @browser = Capybara.current_session
       @browser.visit @url
 
-      collect_nutrition_facts_per_serving("100 grams")
+      collect_nutrition_facts_per_serving("#{@per_serving_amount} #{@per_serving_unit}")
 
       all_nutrition_facts = @browser.all(:xpath, "//*[contains(concat(' ', normalize-space(@class), ' '), ' nf1 ')]/..")
       all_nutrition_facts.collect do |nf|
         name = nf.find_css('.nf1').first.all_text
         amount = nf.find_css('.nf2').first.all_text
         unit = nf.find_css('.nf3').first.all_text
-        NutritionFact.new(name, amount, unit) if amount != "~"
+        @nutrition_facts << NutritionFact.new(name, amount, unit) if(amount != "~")
       end
-      return { per_serving_amount: "100", per_serving_unit: 'g', nutrition_facts: all_nutrition_facts }
     end
 
-    def collect_nutrition_facts_per_serving(amount)
-      @browser.select amount, from: 'serving'
+    def collect_nutrition_facts_per_serving(serving_amount)
+      @browser.select serving_amount, from: 'serving'
     end
   end
 
